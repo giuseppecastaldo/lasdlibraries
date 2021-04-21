1cbd613e041c74a3b8068a5d56a1023292f7a108
Giuseppe Castaldo <peppopiano@gmail.com>
2021-04-21 09:01:18 +0200

    Implementato StackVec

--- lasd/stack/vec/stackvec.cpp
+++ lasd/stack/vec/stackvec.cpp
@@ -1,11 +1,9 @@
-#include <stdexcept>
+namespace lasd {
 
-// Default constructor
+/* ************************************************************************** */
+
 template <typename Data>
-StackVec<Data>::StackVec() : Vector<Data>(2) {
-    // size = 2;
-    // Elements = new Data[2];
-}
+StackVec<Data>::StackVec() : Vector<Data>(2) { }
 
 template <typename Data>
 StackVec<Data>::StackVec(const LinearContainer<Data>& con) {
@@ -17,132 +15,128 @@ StackVec<Data>::StackVec(const LinearContainer<Data>& con) {
     }
 }
 
-// Copy constructor
 template <typename Data>
-StackVec<Data>::StackVec(const StackVec& ref) : Vector<Data>(ref), index(ref.index){}
+StackVec<Data>::StackVec(const StackVec& s) : Vector<Data>(s) { index = s.index; }
 
-// Move constructor
 template <typename Data>
-StackVec<Data>::StackVec(StackVec&& ref) noexcept : Vector<Data>(std::move(ref)) {
-    std::swap(index, ref.index);
-}
+StackVec<Data>::StackVec(StackVec&& s) noexcept : Vector<Data>(std::move(s)) { std::swap(index, s.index); }
 
-// Destructor
 template <typename Data>
-StackVec<Data>::~StackVec(){} // E' chiamato quello di Vector
+StackVec<Data>::~StackVec() { }
 
-
-// Copy assignment
 template <typename Data>
-StackVec<Data>& StackVec<Data>::operator =(const StackVec<Data>& ref){
-    Vector<Data>::operator =(ref);
-    index = ref.index;
+StackVec<Data>& StackVec<Data>::operator=(const StackVec& s) {
+    Vector<Data>::operator=(s);
+    index = s.index;
     return *this;
 }
 
-// Move assignment
 template <typename Data>
-StackVec<Data>& StackVec<Data>::operator =(StackVec<Data>&& ref) noexcept{
-    Vector<Data>::operator =(std::move(ref));
-    std::swap(index, ref.index);
+StackVec<Data>& StackVec<Data>::operator=(StackVec&& s) noexcept {
+    Vector<Data>::operator=(std::move(s));
+    std::swap(index, s.index);
     return *this;
 }
 
-// Comparison operators
 template <typename Data>
-bool StackVec<Data>::operator ==(const StackVec<Data>& ref) const noexcept{
-    if(index != ref.index)
+bool StackVec<Data>::operator==(const StackVec& s) const noexcept {
+    if (index != s.index) {
         return false;
+    }
     
-    for(unsigned long i = 0; i < index; i++){
-        if(Elements[i] != ref.Elements[i])
+    for (unsigned long i = 0; i < index; i++) {
+        if (Elements[i] != s.Elements[i]) {
             return false;
+        }
     }
     
     return true;
 }
 
 template <typename Data>
-bool StackVec<Data>::operator !=(const StackVec<Data>& ref) const noexcept{
-    return !(*this == ref);
+bool StackVec<Data>::operator!=(const StackVec& s) const noexcept {
+    return !(*this == s);
 }
 
-
-// Specific member functions (inherited from Stack)
 template <typename Data>
-Data& StackVec<Data>::Top(){
-    if(index == 0)
-        throw std::length_error("StackVec vuoto!");
+void StackVec<Data>::Push(const Data& data) {
+    if (index == size - 1) {
+        Expand();
+    }
     
-    return Elements[index-1];
+    Elements[index] = data;
+    index++;
 }
 
 template <typename Data>
-void StackVec<Data>::Pop(){
-    if(index == 0)
-        throw std::length_error("StackVec vuoto!");
+void StackVec<Data>::Push(Data&& data) noexcept {
+    if (index == size - 1) {
+        Expand();
+    }
+    
+    std::swap(Elements[index], data);
+    index++;
+}
+
+template <typename Data>
+Data& StackVec<Data>::Top() {
+    if (index == 0) {
+        throw std::length_error("Access to an empty stack vector.");
+    }
+    
+    return Elements[index - 1];
+}
+
+template <typename Data>
+void StackVec<Data>::Pop() {
+    if (index == 0) {
+        throw std::length_error("Access to an empty stack vector.");
+    }
     
     index--;
-    if(index <= (size/4))
+    if (index <= size/4) {
         Reduce();
+    }
 }
 
 template <typename Data>
-Data StackVec<Data>::TopNPop(){
-    if(index == 0)
-        throw std::length_error("StackVec vuoto!");
-    
+Data StackVec<Data>::TopNPop() {
     Data result = Top();
     Pop();
     return result;
 }
 
 template <typename Data>
-void StackVec<Data>::Push(const Data& ref){
-    if(index == size-1)
-        Expand();
-    
-    Elements[index] = ref;
-    index++;
-}
-
-template <typename Data>
-void StackVec<Data>::Push(Data&& ref) noexcept{
-    if(index == size-1)
-        Expand();
-    
-    std::swap(Elements[index], ref);
-    index++;
-}
-
-// Specific member functions (inherited from Container)
-template <typename Data>
-bool StackVec<Data>::Empty() const noexcept{
-    if(index == 0)
+bool StackVec<Data>::Empty() const noexcept {
+    if (index == 0) {
         return true;
+    }
     
     return false;
 }
 
 template <typename Data>
-unsigned long StackVec<Data>::Size() const noexcept{
+unsigned long StackVec<Data>::Size() const noexcept {
     return index;
 }
 
 template <typename Data>
-void StackVec<Data>::Clear(){
-    Vector<Data>::Clear();
+void StackVec<Data>::Clear() {
+    size = 2;
+    Elements = new Data[2];
     index = 0;
-    Vector<Data>::Resize(2);
-}
-
-
-template <typename Data>
-void StackVec<Data>::Expand(){
-    Vector<Data>::Resize(2*size);
 }
 
 template <typename Data>
-void StackVec<Data>::Reduce(){
-    Vector<Data>::Resize(size/2);
+void StackVec<Data>::Expand() {
+    Vector<Data>::Resize(size * 2);
+}
+
+template <typename Data>
+void StackVec<Data>::Reduce() {
+    Vector<Data>::Resize(size / 2);
+}
+
+/* ************************************************************************** */
+
 }
--- lasd/stack/vec/stackvec.hpp
+++ lasd/stack/vec/stackvec.hpp
@@ -1,25 +1,39 @@
 #ifndef STACKVEC_HPP
 #define STACKVEC_HPP
 
+/* ************************************************************************** */
+
 #include "../stack.hpp"
 #include "../../vector/vector.hpp"
 
+/* ************************************************************************** */
+
 namespace lasd {
 
+/* ************************************************************************** */
+
 template <typename Data>
-class StackVec : public Stack<Data>, protected Vector<Data>{
+class StackVec: public Stack<Data>, protected Vector<Data> {
+    
 private:
     
 protected:
+    
     using Vector<Data>::size;
     using Vector<Data>::Elements;
     unsigned long index = 0;
     
 public:
+    
     // Default constructor
     StackVec();
     
+    /* ************************************************************************ */
+    
+    // Specific constructor
     StackVec(const LinearContainer<Data>&);
+    
+    /* ************************************************************************ */
     
     // Copy constructor
     StackVec(const StackVec&);
@@ -27,39 +41,60 @@ public:
     // Move constructor
     StackVec(StackVec&&) noexcept;
     
+    /* ************************************************************************ */
+    
     // Destructor
-    ~StackVec();
+    virtual ~StackVec();
+    
+    /* ************************************************************************ */
     
     // Copy assignment
-    StackVec& operator =(const StackVec&);
+    StackVec& operator=(const StackVec&);
     
     // Move assignment
-    StackVec& operator =(StackVec&&) noexcept;
+    StackVec& operator=(StackVec&&) noexcept;
+    
+    /* ************************************************************************ */
     
     // Comparison operators
-    bool operator ==(const StackVec&) const noexcept;
-    bool operator !=(const StackVec&) const noexcept;
+    bool operator==(const StackVec&) const noexcept;
+    inline bool operator!=(const StackVec&) const noexcept;
+    
+    /* ************************************************************************ */
     
     // Specific member functions (inherited from Stack)
-    Data& Top() override; // Override Stack member (might throw std::length_error)
-    void Pop() override; // Override Stack member (might throw std::length_error)
-    Data TopNPop() override; // Override Stack member (might throw std::length_error)
-    void Push(const Data&) override; // Override Stack member
-    void Push(Data&&) noexcept override; // Override Stack member
+    
+    void Push(const Data&) override;
+    void Push(Data&&) noexcept override;
+    
+    Data& Top() override;
+    
+    void Pop() override;
+    Data TopNPop() override;
+    
+    /* ************************************************************************ */
     
     // Specific member functions (inherited from Container)
-    bool Empty() const noexcept override; // Override Container member
-    unsigned long Size() const noexcept override; // Override Container member
-    void Clear() override; // Override Container member
+    
+    bool Empty() const noexcept override;
+    
+    unsigned long Size() const noexcept override;
+    
+    void Clear() override;
     
 protected:
-    void Expand(); // Accessory function
-    void Reduce(); // Accessory function
+    
+    // Auxiliary member functions
+    
+    void Expand();
+    void Reduce();
     
 };
 
-#include "stackvec.cpp"
+/* ************************************************************************** */
 
 }
+
+#include "stackvec.cpp"
 
 #endif
